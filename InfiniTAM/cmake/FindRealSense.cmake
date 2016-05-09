6c619fc467338b28d7c71dd189fec406e63d83f7
julien.waechter <julien.waechter@gmail.com>
2016-05-09 09:42:30 +0200

    Updated CMake script to find RealSense (removed OpenNI references)

--- InfiniTAM/cmake/FindRealSense.cmake
+++ InfiniTAM/cmake/FindRealSense.cmake
@@ -1,16 +1,16 @@
-# - Find OpenNI
+# - Find RealSense
 # This module defines
-#  OpenNI_INCLUDE_DIR, where to find OpenNI include files
-#  OpenNI_LIBRARIES, the libraries needed to use OpenNI
-#  OpenNI_FOUND, If false, do not try to use OpenNI.
+#  RealSense_INCLUDE_DIR, where to find RealSense include files
+#  RealSense_LIBRARIES, the libraries needed to use RealSense
+#  RealSense_FOUND, If false, do not try to use RealSense.
 # also defined, but not for general use are
-#  OpenNI_LIBRARY, where to find the OpenNI library.
+#  RealSense_LIBRARY, where to find the RealSense library.
 
-set(REALSENSE_ROOT "/usr/local" CACHE FILEPATH "Root directory of librealsense")
+set(REALSENSE_ROOT "/usr/local" CACHE PATH "Root directory of librealsense")
 
-#  FIND_PATH(RealSense_ROOT librealsense.vc12 HINTS "D:/Develop/intel/librealsense")
-FIND_PATH(RealSense_INCLUDE_DIR librealsense HINTS "${REALSENSE_ROOT}/include")
-FIND_LIBRARY(RealSense_LIBRARY realsense HINTS "${REALSEMSE_ROOT}/bin/x64" "${REALSENSE_ROOT}/lib")
+#  find_path(RealSense_ROOT librealsense.vc12 HINTS "D:/Develop/intel/librealsense")
+find_path(RealSense_INCLUDE_DIR librealsense HINTS "${REALSENSE_ROOT}/include")
+find_library(RealSense_LIBRARY realsense HINTS "${REALSEMSE_ROOT}/bin/x64" "${REALSENSE_ROOT}/lib")
 
 # handle the QUIETLY and REQUIRED arguments and set REALSENSE_FOUND to TRUE if
 # all listed variables are TRUE
@@ -18,9 +18,9 @@ FIND_LIBRARY(RealSense_LIBRARY realsense HINTS "${REALSEMSE_ROOT}/bin/x64" "${RE
 #include(${CMAKE_MODULE_PATH}/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(RealSense DEFAULT_MSG RealSense_LIBRARY RealSense_INCLUDE_DIR)
 
-#if(OPENNI_FOUND)
-#  set(OpenNI_LIBRARIES ${OpenNI_LIBRARY})
-#endif()
+if(REALSENSE_FOUND)
+  set(RealSense_LIBRARIES ${RealSense_LIBRARY})
+endif()
 
 mark_as_advanced(RealSense_LIBRARY RealSense_INCLUDE_DIR)
 
